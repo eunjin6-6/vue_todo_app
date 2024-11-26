43b9fdbaeb93224abde0182138e59dacb6585dad
eunjin6-6 <dmswls616@gmail.com>
2024-11-26 16:53:44 +0900

    할일 목록 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,53 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { reactive, ref } from 'vue';
+import Todo from './components/TodoItem.vue';
 
+const todoText = ref('');
+
+const addtodo = (e) =>{
+  todoText.value = e.target.value;
+  //빈칸없애기. 대상.trim();
+  const todoTextLength = todoText.value.trim().length;
+  
+  if(todoTextLength > 0){
+    todos.push({id: todoTextLength+1 , title: todoText.value, checked : false})  
+  }
+
+  todoText.value = '';
+}
+
+const todos = reactive([
+  {id: 1, title: '웹 배우기', checked : false},
+  {id: 2, title: '취업하기', checked : false}
+])
+
+const toggleCheckbox = (id, checked)=>{
+  console.log(id, checked, todos.values);
+  const idx = todos.findIndex(todo=> todo.id === id);
+  todos[idx].checked = checked
+}
 </script>
 
 <template>
-  <div id="app" class="container">
-    <h1>Vue Todo App</h1>
-  </div>
+ <div id="app" class="container">
+  <h1>Vue Todo App</h1>
+
+  <!-- 
+  입력 내용 실시간 반영
+  <BFormInput placeholder="할일을 입력하세요" v-model="todoText" />
+  <Todo :todo="todoText" /> -->
+
+  <!-- 입력 후 엔터 시 내용 추가 -->
+  <BFormInput placeholder="할일을 입력하세요" @keyup.enter="addtodo" v-model="todoText"/>
+  <Todo 
+    v-for="todo in todos" 
+    :key="todo.id" 
+    :todo="todo" 
+    @toggle-checkbox="toggleCheckbox"
+  />
+
+ </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  todo: {
+    type: String,
+    required: true,
+  },
+})
+
+
+const emit = defineEmits(['toggle-checkbox'])
+
+const toggleCheckbox = (e)=>{
+  emit('toggle-checkbox',{
+    id: props.todo.id,
+    checked: e.target.checked
+  })
+}
+
+
+</script>
+
+<template>
+  <div>
+    <BFormCheckbox
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+      @change="toggleCheckbox"
+      >
+      <!-- :class="['tab-button', { active: currentTab === tab }]" -->
+      <span :class="todo.checked && 'muted'">{{ todo.title }}</span> 
+    </BFormCheckbox>
+  </div>
+</template>
+
+<style>
+  .muted{
+    text-decoration: line-through;
+  }
+</style>
+
