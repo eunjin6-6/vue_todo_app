1eeca8fd51d6604cfe3880b8dc3c6be652c3770e
eunjin6-6 <dmswls616@gmail.com>
2024-11-26 16:53:30 +0900

    부트스트랩 설치

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
     vueJsx(),
     vueDevTools(),
   ],
